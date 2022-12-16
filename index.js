1a91df3e73a780724e63222bcc2644f3cc1685c2
w3villa-bedprakash <bedprakash.kushwaha@w3villa.com>
2022-12-16 23:08:12 +0530

    upload image

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const redis = require("redis");
+var request = require('request').defaults({ encoding: null });
+
 const redisClient = redis.createClient(
     {
         socket: {
@@ -12,7 +14,8 @@ const redisClient = redis.createClient(
 	await redisClient.connect();
 })();
 
-console.log("Connecting to the Redis");
+const url = 'https://raw.githubusercontent.com/vedprakashkushwaha/convert-base64/main/World_cat.webp'
+
 
 redisClient.on("ready", () => {
 	console.log("Connected!");
@@ -22,12 +25,49 @@ redisClient.on("error", (err) => {
 	console.log("Error in the Connection");
 });
 
-
 async function setData(key, value) {
-    await redisClient.set(key, value, (err, reply) => {
-        logger.info('Data set into cache: ', key, reply);
-    });
+    try {
+        await redisClient.set(key, value, (err, reply) => {
+            if (err) {
+                console.log("error while setting data: ", err);
+            }
+            console.log('Data set into cache: ', key, reply);
+        });
+        console.log('Setting Key ', key);
+    } catch (err) {
+        console.log("Error while setting: ", err);
+    };
 }
 
 
-setData('test-key-ved', "this is a test key");
+function convertImageToBase64(url) {
+    try {
+        return new Promise((resolve, reject) => {
+            request.get(url, async function (error, response, body) {
+              if (error || response.statusCode !== 200) resolve({ status: response.statusCode, error: error });
+              if (!error && response.statusCode == 200) {
+                data = Buffer.from(body).toString('base64');
+                resolve({ status: response.statusCode, data: data });
+              }
+            });
+          });
+    } catch (err) {
+        console.log("Error while fetching data: ", err)
+    }
+    
+  }
+
+async function getImageString() {
+   const data =  await convertImageToBase64(url);
+   await setData('world_cup_missing_image', data.data);
+}
+getImageString()
+// async function convertIntoBase4(url) {
+//     const response = await fetch(url);
+//     const buffer = await response.buffer();
+//     const base64 = buffer.toString('base64');
+//     const base4 = base64.replace(/\+/g, '-').replace(/\//g, '_');
+//     return await redisClient.get(key);
+// }
+
+// setData('test-key-ved', "this is a test key");
